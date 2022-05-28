a9ce737840fa3067ee426a7532a6c432e9228ba4
ik7719 <imlogic@naver.com>
2022-05-28 21:16:50 +0900

    if 삭제 -> boolean으로 단순화

--- src/main/java/com/homework/week04/domain/Web.java
+++ src/main/java/com/homework/week04/domain/Web.java
@@ -1,8 +1,12 @@
 package com.homework.week04.domain;
 
+import lombok.Getter;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
 public class Web
 {
     @Id
@@ -14,4 +18,10 @@ public class Web
 
     @Column(nullable = false)
     private String password;
+
+    public boolean checkPassword(WebRequestDto webRequestDto)
+    {
+        password.equals(WebRequestDto.getPassword());
+        return true;
+    }
 }
---
+++ src/main/java/com/homework/week04/domain/WebRequestDto.java
@@ -0,0 +1,11 @@
+package com.homework.week04.domain;
+
+import lombok.Getter;
+
+@Getter
+public class WebRequestDto
+{
+    private String nickName;
+    private String password;
+    private String checkPassword;
+}
